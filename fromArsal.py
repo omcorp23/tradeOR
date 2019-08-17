1ab250693ec7709cf349f6e66d954dbf60c22c01
omcor <ghfj>
2019-08-17 19:02:55 +0300

    first 17.8

--- fromArsal.py
+++ fromArsal.py
@@ -15,11 +15,11 @@ while order==False:
     BTC= client.get_historical_klines(symbol=symbol, interval='30m', start_str="1 hour ago UTC")
     print(BTC[-1][4])
     print(BTC[-2][4])
-    if (float(BTC[-1][4])-float(BTC[-2][4]))>5:
+    if (float(BTC[-1][4])-float(BTC[-2][4])) > 5:
         print ('Buyyy')
         #client.order_market_buy(symbol= symbol, quantity= quantity)
         #order= True
-    elif (float(BTC[-1][4])-float(BTC[-2][4]))<-5:
+    elif (float(BTC[-1][4])-float(BTC[-2][4])) < -5:
         print ('Sellll')
         #client.order_market_buy(symbol= symbol , quantity= quantity)
         #order= True
