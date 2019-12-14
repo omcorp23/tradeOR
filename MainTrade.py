450bd8dfafb29bfd9f32b2efe37dbb18cebbcafd
omcor <ghfj>
2019-12-14 18:37:56 +0200

    now big_market works - changed 'BTC/USD' to 'BTC/USDT'. doesn't works for small_market

--- MainTrade.py
+++ MainTrade.py
@@ -25,7 +25,11 @@ def run():
     coins = ['BTC/USD'] # TODO: get all coins relevant for this
 
     # Extract candles
-    big_market.ohlcv = small_market.exchange.fetch_ohlcv(coins[0], trading_window.candle_time_frame, small_market.exchange.parse8601(trading_window.start_time))
+    big_market.ohlcv = big_market.exchange.fetch_ohlcv('BTC/USDT', trading_window.candle_time_frame, big_market.exchange.parse8601(trading_window.start_time))
+    print(big_market.ohlcv)
+
+    small_market.ohlcv = small_market.exchange.fetch_ohlcv('BTC/USDC', trading_window.candle_time_frame,
+                                                       small_market.exchange.parse8601(trading_window.start_time))
     print(small_market.ohlcv)
 
 
